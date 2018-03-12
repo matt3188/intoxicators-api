4a554b5d9169c1548491ab3bef47c39d171d73ca
Matt Coleman <matt.coleman@thisisdare.com>
2018-03-12 10:24:48 +0000

    Implemented more endpoints

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log('ERROR:' + reason);
+  console.log('ERROR: ' + reason);
   res.status(code || 500).json({ error: message });
 }
 
@@ -44,7 +44,18 @@ function handleError(res, reason, message, code) {
  *    POST: creates a new contact
  */
 
-app.get('/contacts', function(req, res) {});
+app.get('/contacts', function(req, res) {
+  db
+    .collection(CONTACTS_COLLECTION)
+    .find({})
+    .toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, 'Failed to get contacts.');
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+});
 
 app.post('/contacts', function(req, res) {
   var newContact = req.body;
@@ -69,8 +80,35 @@ app.post('/contacts', function(req, res) {
  *    DELETE: deletes contact by id
  */
 
-app.get('/contacts/:id', function(req, res) {});
+app.get('/contacts/:id', function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, 'Failed to get contact');
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
 
-app.put('/contacts/:id', function(req, res) {});
+app.put('/contacts/:id', function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
 
-app.delete('/contacts/:id', function(req, res) {});
+  db.collection(CONTACTS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, 'Failed to update contact');
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+app.delete('/contacts/:id', function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function(err, result) {
+    if (err) {
+      handleError(res, err.message, 'Failed to delete contact');
+    } else {
+      res.status(204).end();
+    }
+  });
+});
